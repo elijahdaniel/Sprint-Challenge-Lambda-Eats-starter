72fc321b6ccb3160ca8edbfb5b8bf2d562c2d9bc
Elijah Paminsan <e.paminsan@gmail.com>
2020-05-22 08:59:03 -0700

    routes and navbar built out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React from 'react'
+import { Route, NavLink, Switch } from 'react-router-dom'
+import Form from './components/Form'
+import Home from './components/Home'
+
+import './style.scss'
 
 const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
-export default App;
+	return (
+		<Route>
+			<div>
+				<nav>
+					<NavLink className='navA' to='/'>
+						Home
+					</NavLink>
+					<NavLink className='navA' to='/pizza'>
+						Pizza
+					</NavLink>
+				</nav>
+
+				<Switch>
+					<Route exact path='/'>
+						<Home />
+					</Route>
+					<Route path='/pizza'>
+						<Form />
+					</Route>
+				</Switch>
+			</div>
+		</Route>
+	)
+}
+export default App
